e8a9d375897da0e2db3c56c52635f43ad7731d54
shrey-creator <60284082+shrey-creator@users.noreply.github.com>
2022-01-03 20:34:01 +0530

    made the code modular and commit ready

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express =require('express');
 const app =express();
 var path = require('path');
 const socket=require('socket.io');
+
+                                                // Routes
 app.use(express.static(__dirname+'/public'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/views/index.html'));
@@ -20,83 +22,93 @@ app.get('/sitemap', function(req, res){
     res.contentType('application/xml');
     res.sendFile(path.join(__dirname , 'sitemap.xml'));
 });
-var rooms=new Map();
-let roomNames=[];
-// const roomNames = new Set();
-var passedNumber=[];
-var number=[];
+
+
+
+                                    // passing numbers logic
+
+var roomCount=new Map();            //stores number of users in each room
+var room_numbers={};                //stores object of key(room names) and value(array of passed numbers) 
+let roomNames=[];                   //stores room Names which is passed to the client to check if room already exists
+
 app.get('/checkRoom', function(req, res) {
     console.log(roomNames);
     res.send(roomNames);
 });
 var server=app.listen((process.env.PORT || 5000),()=>{console.log("app started on localhost 5000")});
 var io=socket(server);
+
+
 io.on('connection',(socket)=>{  
     var currentRoom;
    console.log('user connected ');
 
+    //  evokes when a new user join the room
     socket.on('join',({room,type})=>{
-        roomNames.push(room);
-        console.log(type);
-        if(!(rooms.get(room)))
+        // check if room already exists
+       
+        if(!(room in room_numbers))
         {
-            rooms.set(room,1);
+        roomNames.push(room);
+        }
+
+        // creating key value of object if key does not exists
+        room_numbers[room] = room_numbers[room] || [];
+        passedNumber=room_numbers[room];
+
+        //set room Count
+        if(!(roomCount.get(room)))
+        {
+            roomCount.set(room,1);
         }
         else{
-            np=rooms.get(room);
-            rooms.set(room,np+1);
+            np=roomCount.get(room);
+            roomCount.set(room,np+1);
         }
-        console.log(rooms);
+        
         currentRoom=room;
-       // console.log(number);
-        passedNumber=[];
-        for( i=0;i<number.length;i++)
-        {           if(number[i].room === room)
-                    {
-                        passedNumber.push(number[i].ran);
-                    }
-        }
-       
         socket.join(room);
        socket.emit('join',passedNumber);
 
     });
-    
 
-    socket.on('number',(data)=>{
-        number.push(data);
-        
     
+    //listening to numbers from client and emitting to all other clients
+    socket.on('number',(data)=>{    
+        room_numbers[data.room].push(data.ran);
         io.to(data.room).emit('number',data);
     });
 
 
+    //listening to chat messages from client and emitting to all other clients
     socket.on('chat', function(data){
-        // console.log(data.message);
         io.to(data.roomna).emit('chat', data);
     });
 
 
+    //evokes when a user get disconnected
     socket.on('disconnect', () => {
-         usersLeft=rooms.get(currentRoom);
+         usersLeft=roomCount.get(currentRoom);
+         console.log(usersLeft);
+         //check if this is the last user in the room
          if(usersLeft-1==0)
          {
-             console.log(currentRoom);
-             rooms.delete(currentRoom);
-             end=number.length;
-             i=0;
-             while( i<end)
-        {           if(number[i].room === currentRoom)
-                    {
-                        number.splice(i,1);
-                        i--;
-                    }
-                    end=number.length;
-                    i++;
-        }
+    
+            for( var i = 0; i < roomNames.length; i++){ 
+                                           
+                if ( roomNames[i] === currentRoom) { 
+                    roomNames.splice(i, 1); 
+                    break;
+                    i--; 
+                }
+            }
+             //if its the last user delete entry from roomCount  and room_numbers
+             roomCount.delete(currentRoom);
+             delete room_numbers[currentRoom]
+             
          }
          else{
-             rooms.set(currentRoom,usersLeft-1);
+             roomCount.set(currentRoom,usersLeft-1);
          }
          console.log('user disconnected');
 
@@ -104,3 +116,6 @@ io.on('connection',(socket)=>{
     
 
 })
+
+
+//TODO delete roomNames who are not present
